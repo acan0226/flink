9a9188aeea256df07cb3e75569a7656bbe3026cb
acan0226 <acan0226@163.com>
2022-03-01 20:28:34 +0800

    git

--- src/main/java/net/acan/net/acan/flink/chapter02/wc_bound.java
+++ src/main/java/net/acan/net/acan/flink/chapter02/wc_bound.java
@@ -1,9 +1,17 @@
 package net.acan.net.acan.flink.chapter02;
 
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
 public class wc_bound {
     public static void main(String[] args) {
-        System.out.println("acan");
-        System.out.println("can");
-        System.out.println("a");
+        //1/创建一个流的执行环境
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(1);
+        //2/通过env得到一个数据流
+        DataStreamSource<String> dataStreamSource = env.socketTextStream("hadoop162", 9999);
+
+        //3/各种转换
+
     }
 }
