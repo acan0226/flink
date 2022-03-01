6d4bea261d6ef0d33bcf70291519ea4efe2bb12e
acan0226 <acan0226@163.com>
2022-03-01 09:00:14 +0800

    git

--- src/main/java/net/acan/net/acan/flink/chapter02/wc_bound.java
+++ src/main/java/net/acan/net/acan/flink/chapter02/wc_bound.java
@@ -4,5 +4,6 @@ public class wc_bound {
     public static void main(String[] args) {
         System.out.println("acan");
         System.out.println("can");
+        System.out.println("a");
     }
 }
